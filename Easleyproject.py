f6c47eaf824a3414434e692ad5c6da546df708c3
Harper Easley <harper.easley@gmail.com>
2023-08-24 16:59:48 +0000

    1st commit

---
+++ Easleyproject.py
@@ -0,0 +1,9 @@
+print('Welcome to the Space Travel Calculator')
+
+distance = int(input("How far are you trying to travel"))
+
+speed = int(input("How fast does your ship go"))
+
+time = distance/speed
+
+print("it will take you,", time, "years to reach where you want to go at your speed")
